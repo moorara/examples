51f09216d5c932cbdab5587c4eaab359667001b0
Milad Irannejad <moorara@gmail.com>
2018-04-27 12:51:38 -0400

    Add methods example

---
+++ rust/tutorials/20-methods/src/main.rs
@@ -0,0 +1,31 @@
+#![allow(dead_code)]
+
+struct Point {
+  x: f64,
+  y: f64
+}
+
+struct Line {
+  start: Point,
+  end: Point
+}
+
+impl Line {
+  fn len(&self) -> f64 {
+    let dx = self.end.x - self.start.x;
+    let dy = self.end.y - self.start.y;
+    (dx * dx + dy * dy).sqrt()
+  }
+}
+
+fn methods() {
+  let p1 = Point{x: 3.0, y: 4.0};
+  let p2 = Point{x: 5.0, y: 10.0};
+  let line = Line{start: p1, end: p2};
+
+  println!("line length is {}", line.len());
+}
+
+fn main() {
+  methods();
+}
